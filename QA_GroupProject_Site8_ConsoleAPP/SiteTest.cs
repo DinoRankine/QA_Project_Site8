4882296ff797631b3fdadd6d756b3039d7a3ac16
Kate Daley <mkatedaley@gmail.com>
2022-04-07 20:20:06 -0300

    Basic Setup
    Added basic setup code and comments to program.cs, created SiteTest.cs and added basic setup code and comments

---
+++ QA_GroupProject_Site8_ConsoleAPP/SiteTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace QA_FinalProject
+{
+    class SiteTests
+    {
+        public static Boolean Test1(IWebDriver driver)
+        {
+
+        }
+
+        static void fillForm(IWebDriver driver)
+        {
+            //Load the Sign Up form page
+            driver.Url = "http://47.55.247.242/site8/signup.php";
+
+            //First Name
+
+            //Last Name
+
+            //Email
+
+            //Screen Name
+
+            //Password
+
+            //Confirm password
+
+            //Phone
+
+            //Address
+
+            //Province
+
+            //Postal Code
+
+            //URL
+
+            //Description
+
+            //Location
+
+            //Click Register button
+            IWebElement btnRegister = driver.FindElement(By.Id("button"));
+            btnRegister.Click();
+        }
+    }
+}
+
