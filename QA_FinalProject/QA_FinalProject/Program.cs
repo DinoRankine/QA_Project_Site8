f23783af8b3364d93d72c296889a493da79a1e5e
Kate Daley <mkatedaley@gmail.com>
2022-04-07 20:11:55 -0300

    First commit
    Added basic setup code and comments to program.cs, created SiteTests.cs and added some basic setup code / comments

---
+++ QA_FinalProject/QA_FinalProject/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace QA_FinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Web Driver
+            IWebDriver driver = new ChromeDriver(@"C:\Selenium");
+
+            //Test #1: Sign Up Page... Check that first name cannot be blank
+            Console.WriteLine("Test #1: Check that first name cannot be blank while signing up for an account");
+            Boolean blnTest01;
+            blnTest01 = SiteTests.Test1(driver);
+            TestResult(blnTest01);
+
+
+            //Test #2
+
+            //Test #3
+
+            //Test #4
+
+            //Test #5
+
+            //Test #6
+
+            //Test #7
+
+            //Test #8
+
+            //Test #9
+
+            //Test #10
+
+            //Test #11
+
+            //Test #12
+
+            //Test #13
+
+            //Test #14
+
+            //Test #15
+
+            
+        }
+
+        static void TestResult(Boolean blnResult)
+        {
+            if (blnResult)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
