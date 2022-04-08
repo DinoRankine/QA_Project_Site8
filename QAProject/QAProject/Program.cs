4649289a789e0cf425c4e1502b2bb62edf1246c3
kerrington-curtis <kcurtis04@mynbcc.ca>
2022-04-08 13:58:26 -0300

    Deleted old project and added new project

---
+++ QAProject/QAProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace QAProject
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // Reset site
+            SiteTest.SiteReset();
+        }
+    }
+}
---
+++ QAProject/QAProject/SiteTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace QAProject
+{
+    class SiteTest
+    {
+        private static MySqlConnection connection;
+
+        public static void SiteReset()
+        {
+            string myConnectionString = "server=47.55.247.242;database=bitter-site8;uid=nick;pwd=asdf;";
+            connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = connection;
+            command.CommandText = "reset";
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+    }
+}
